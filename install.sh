a92f769fbd642fddc146d6a895678a37b69d2772
Guiyun <guiyun@GuiyundeMacBook-Pro.local>
2018-02-01 22:52:57 +0800

    修改

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # apt-get update;
 
 # #安装
-apt-get install -y build-essential git gcc g++ make systemtap-sdt-dev libxml2-dev zlib1g-dev libbz2-dev libcurl4-gnutls-dev libwebp-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libvpx-dev libldap2-dev
+apt-get install -y build-essential git gcc g++ make systemtap-sdt-dev libxml2-dev zlib1g-dev libbz2-dev libcurl4-gnutls-dev libwebp-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libvpx-dev libldap2-dev libsasl2-dev
 
 # #下载php
 # wget http://jp2.php.net/distributions/php-7.1.13.tar.gz
@@ -25,9 +25,9 @@ apt-get install -y build-essential git gcc g++ make systemtap-sdt-dev libxml2-de
 
 cd php-7.1.13
 
-ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/
+# ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/
 
-ln -s /usr/lib/x86_64-linux-gnu/liblber.so /usr/lib/
+# ln -s /usr/lib/x86_64-linux-gnu/liblber.so /usr/lib/
 
 ./configure --prefix=/usr/local/php --enable-bcmath --enable-calendar --enable-dba --enable-exif --enable-ftp --enable-gd-native-ttf --enable-mbregex --enable-mbstring --enable-shmop --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-wddx --enable-zip --with-freetype-dir --with-gd --with-iconv-dir --with-icu-dir --with-jpeg-dir --with-mhash --with-png-dir --with-xmlrpc --with-zlib --with-readline --without-snmp --with-libxml-dir --with-unixODBC=/usr --with-bz2 --enable-debug --with-openssl --enable-fpm --with-fpm-user=_www --with-fpm-group=_www --with-curl --with-xsl --with-ldap --with-ldap-sasl --with-mysql-sock --with-mysqli --with-pdo-mysql=mysqlnd --disable-opcache --enable-pcntl --without-pear --enable-dtrace --disable-phpdbg --enable-zend-signals 
 
