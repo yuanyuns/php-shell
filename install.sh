31f2db23af43959dfa3968e45928a40cf9874cc7
Guiyun <guiyun@GuiyundeMacBook-Pro.local>
2018-02-16 20:09:55 +0800

    解决若干bug

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 apt-get update;
 
 # #安装
-apt-get install -y vim build-essential git gcc g++ make systemtap-sdt-dev libxml2-dev zlib1g-dev libbz2-dev libcurl4-gnutls-dev libwebp-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libvpx-dev libldap2-dev libsasl2-dev unixodbc-dev libedit-dev libreadline-dev libxslt1-dev
+apt-get install -y vim build-essential git gcc g++ make systemtap-sdt-dev libxml2-dev zlib1g-dev libbz2-dev libcurl4-gnutls-dev libwebp-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libvpx-dev libldap2-dev libsasl2-dev unixodbc-dev libedit-dev libreadline-dev libxslt1-dev libpq-dev pgadmin3 postgresql postgresql-client
 
 echo "-----------------安装php--------------------------"
 
@@ -32,7 +32,6 @@ ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/
 ln -s /usr/lib/x86_64-linux-gnu/liblber.so /usr/lib/
 
 ./configure --prefix=/usr/local/php --enable-bcmath --enable-calendar --enable-dba --enable-exif --enable-ftp --enable-gd-native-ttf --enable-mbregex --enable-mbstring --enable-shmop --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-wddx --enable-zip --with-freetype-dir --with-gd --with-iconv-dir --with-icu-dir --with-jpeg-dir --with-mhash --with-png-dir --with-xmlrpc --with-zlib --with-readline --without-snmp --with-libxml-dir --with-unixODBC=/usr --with-bz2 --enable-debug --with-openssl --enable-fpm --with-fpm-user=_www --with-fpm-group=_www --with-curl --with-xsl --with-ldap --with-ldap-sasl --with-mysql-sock --with-mysqli --with-pdo-mysql=mysqlnd --disable-opcache --enable-pcntl --without-pear --enable-dtrace --disable-phpdbg --enable-zend-signals --with-pdo_pgsql --enable-opcache
-
 
 
 make && make install
